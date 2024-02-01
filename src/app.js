f58e743e9fbdce10417b3073e2cab30a16a917fb
Santiago Diaz Muñoz <149324836+Diaz1010@users.noreply.github.com>
2024-02-01 23:44:42 +0000

    Version1

--- src/app.js
+++ src/app.js
@@ -7,5 +7,22 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let figureOne = document.getElementById("figureOne");
+  let figureTwo = document.getElementById("figureTwo");
+  let number = document.getElementById("number");
+
+  let arrayFigures = ["♦", "&#9829;", "♠ ", "♣"];
+  let arrayNumber = [2, 3, 4, 5, 6, 7, 8, 9, 10, "A", "J", "Q", "K"];
+
+  let randomFigures =
+    arrayFigures[Math.floor(Math.random() * arrayFigures.length)];
+  let randomNumber =
+    arrayNumber[Math.floor(Math.random() * arrayNumber.length)];
+  if (randomFigures == "&#9829;" || randomFigures == "♦") {
+    figureTwo.style.color = "red";
+    figureOne.style.color = "red";
+  }
+  figureOne.innerHTML = randomFigures;
+  figureTwo.innerHTML = randomFigures;
+  number.innerHTML = randomNumber;
 };
